dea36313654890554162c79235f93ce036abb078
Jose del Carmen Gomez Madrigal <35632994+Chkm64@users.noreply.github.com>
2023-11-09 12:32:07 -0500

    Integrar filtros en Records

--- composables/api/useRecords.js
+++ composables/api/useRecords.js
@@ -3,9 +3,6 @@ export default function () {
   async function getRecords(link_id) {
     const { data, error } = await supabase
       .from("view_records")
-      // .from("records")
-      // .select("*, codes!inner(*, rel_users_to_organizations!inner(*))")
-      // .select('*, codes!inner(link_id, rel_users_to_organizations!inner(profile_id, profiles!inner(*)))')
       .select()
       .eq("rel_id", link_id);
     if (error) throw error;
@@ -18,20 +15,8 @@ export default function () {
     return data;
   }
 
-  async function getRecordsByDates(link_id, dates) {
-    const { data, error } = await supabase
-      .from("records")
-      .select('*, codes!inner(link_id, rel_users_to_organizations!inner(profile_id, profiles!inner(*)))')
-      .gt('created_at', dates.date_start)
-      .lt('created_at', dates.date_end)
-      .eq("codes.rel_users_to_organizations.id", link_id);
-    if (error) throw error;
-    return data;
-  }
-
   return {
     getRecords,
     createRecords,
-    getRecordsByDates,
   };
 }
--- pages/admin/records/index.vue
+++ pages/admin/records/index.vue
@@ -10,11 +10,17 @@ import useApi from "@/composables/useApi";
 const { getRecords } = useApi();
 const link_id = ref();
 const records = ref([]);
+const getFormatDate = (data) => {
+  return [...(data || [])].map((d) => {
+    d.created_at = new Date(d.created_at);
+    return d;
+  });
+};
 
 onMounted(async () => {
   link_id.value = await JSON.parse(localStorage.getItem("sb_org_id"));
-
-  records.value = await getRecords(link_id.value.code);
+  // records.value = await getRecords(link_id.value.code);
+  records.value = getFormatDate(await getRecords(link_id.value.code));
 });
 </script>
 <style scoped></style>
